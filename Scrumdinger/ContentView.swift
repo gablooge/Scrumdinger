42057605aec4bbce979734540221666feec1ae70
Samsul Hadi <samsulhadikediri@gmail.com>
2021-07-29 06:31:18 +0700

    Create Project

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Samsul Hadi on 29/07/21.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                  Text("Second Elapsed")
+                    .font(.caption)
+                  Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                Text("Second Remaining")
+                    .font(.caption)
+                Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel(Text("time remaining"))
+            .accessibilityValue(Text("10 minutes"))
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Samsul Hadi on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
